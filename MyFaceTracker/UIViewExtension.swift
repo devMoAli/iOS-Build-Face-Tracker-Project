264210fc6e381625efb96f11da400dd56b44cb80
MoooAli <mohamed.ali.uk2020@gmail.com>
2022-03-23 14:01:12 +0000

    iOS

---
+++ MyFaceTracker/UIViewExtension.swift
@@ -0,0 +1,22 @@
+
+import UIKit
+
+extension UIView {
+    func setAnchorPoint(_ anchorPoint: CGPoint) {
+        var newPoint = CGPoint(x: bounds.size.width * anchorPoint.x, y: bounds.size.height * anchorPoint.y)
+        var oldPoint = CGPoint(x: bounds.size.width * layer.anchorPoint.x, y: bounds.size.height * layer.anchorPoint.y)
+        
+        newPoint = newPoint.applying(transform)
+        oldPoint = oldPoint.applying(transform)
+        
+        var position = layer.position
+        position.x -= oldPoint.x
+        position.x += newPoint.x
+        
+        position.y -= oldPoint.y
+        position.y += newPoint.y
+        
+        layer.position = position
+        layer.anchorPoint = anchorPoint
+    }
+}
